a991716174d5f2848c9c11e85cbef77c6825e6a4
hrishik18 <hrishik.s@somaiya.edu>
2021-12-30 18:43:51 +0530

    Motion box trials

--- pages/index.js
+++ pages/index.js
@@ -4,15 +4,24 @@ import { motion } from 'framer-motion'
 const MotionBox = motion(Box)
 
 function Home() {
+    const list = { hidden: { opacity: 0 } }
+    const item = { hidden: { x: -10, opacity: 0 } }
     return (
         <MotionBox
-            height='40px'
-            bg='red.300'
+            variants={list}
+            height='100px'
+            bg='green.300'
             drag='x'
-            dragConstraints={{ left: -300, right:150 }}
+            dragConstraints={{ left: -500, right: 150 }}
             whileHover={{ scale: 1.1 }}
             whileTap={{ scale: 0.9 }}
-        />
+            >
+            <motion.li variants={item}   />
+            <motion.li variants={item} />
+            <motion.li variants={item} />
+            </MotionBox>
+            
+        
     )
 }
 export default Home;
