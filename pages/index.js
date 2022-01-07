481b66f6688444a7c96bfb9c310c55784f3d3de7
hrishik18 <hrishik.s@somaiya.edu>
2022-01-07 20:09:57 +0530

    Plan

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,15 @@ import { Box } from '@chakra-ui/react'
 function Home() {
     return (
         <Box>
-            
+        //navbar 
+        //Header
+        // Display pic
+        //Programmer,.....//useless boi
+        // about me
+        // Tech stacks 
+        // Achievements lmao
+        // Dummy Projects info
+        // footer
         </Box>
     )
 }
